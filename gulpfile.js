f9274070977f5602deaa97f7ff5945f4ee986148
Shanaun <sclayton1183@gmail.com>
2019-03-07 04:21:48 -0800

    testting

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
 const autoprefixer = require('gulp-autoprefixer')
-// const concat = require('gulp-concat')
-// const babel = require('gulp-babel')
-// const watch = require('gulp-watch')
+const concat = require('gulp-concat')
+const babel = require('gulp-babel')
+const watch = require('gulp-watch')
 const browserSync = require('browser-sync')
 const reload = browserSync.reload
 var exec = require('child_process').exec;
@@ -28,12 +28,22 @@ gulp.task('styles', () => {
     .pipe(browserSync.stream())
 })
 
-gulp.task('server', gulp.series('build', function () {
-  browser.init({
-    server: './_site',
-    port: port
-  });
-}));
+// gulp.task('browser-sync', ['styles'], function () {
+  // THIS IS FOR SITUATIONS WHEN YOU HAVE ANOTHER SERVER RUNNING
+  // browserSync.init({
+  //   proxy: {
+  //     target: 'localhost:3000', // can be [virtual host, sub-directory, localhost with port]
+  //     ws: true // enables websockets
+  //   },
+  //   serveStatic: ['.', './public']
+  // })
+
+  browserSync.init({
+        server: './public',
+        notify: false,
+        open: false //change this to true if you want the broser to open automatically 
+    });
+})
 
 gulp.task('webpack', (cb) => {
   exec('webpack', function (err, stdout, stderr) {
@@ -43,11 +53,11 @@ gulp.task('webpack', (cb) => {
     });
 })
 
-// gulp.task('webpack', shell.task([
-//   'webpack'
-// ]))
+gulp.task('webpack', shell.task([
+  'webpack'
+]))
 
-// gulp.task('server', shell.task([
-//   'yarn run server'
-// ]))
+gulp.task('server', shell.task([
+  'yarn run server'
+]))
 
