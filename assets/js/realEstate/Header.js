6d24f9435b844645a61dd0f88b299b5a635f958c
Shanaun <sclayton1183@gmail.com>
2019-03-11 00:35:23 -0700

    changed app title

--- assets/js/realEstate/Header.js
+++ assets/js/realEstate/Header.js
@@ -10,16 +10,15 @@ export default class Header extends Component {
   }
   render () {
     return (<header>
-      <div className="logo">Cool Real Estate Logo</div>
+      <div className="logo">Real Estate Listings</div>
 
       <nav>
         <a href="#">Create Ads</a>
         <a href="#">About Us</a>
         <a href="#">Log In</a>
         <a href="#" className="register-btn">Register</a>
-      </nav>  
-      
+      </nav>
+
     </header>)
   }
 }
-
